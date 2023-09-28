88a345484b6c53f993096787216ce3a7dde73ec3
Ashishkumar Patil <ashishkumar.patil@dragonflyft.com>
2023-09-28 16:37:02 +0530

    Added new model Audit Details

---
+++ models/uobData/AuditDetails.sql
@@ -0,0 +1,33 @@
+{{ config(materialized='table') }}
+
+with uob_audit as (
+    select * from airbytescm.CORAUDIT
+), 
+ uob_audittype as (
+    select * from airbytescm.CORAUDITTYPE
+ ),
+ uob_message as (
+    select * from airbytescm.ARCMESSAGE
+ ),
+
+ DNAuditDetails as (
+    select uob_audit.LOGINID,
+        uob_audit.ENTERPRISECUSTID,
+        uob_audit.AUDITDTTM,
+        uob_audit.SESSIONID,
+        uob_audit.CORORGKYFI,
+        uob_audit.IPADDRESS,
+        uob_audit.TRNREFNUMBER,
+        uob_audittype.ARCCHANNELKY,
+        uob_audittype.APPCOMPONENTENUM,
+        uob_audittype.AUDITFUNCTIONENUM,
+        uob_message.MSGCODE,
+        uob_message.TRANSLATION
+
+        From uob_audit 
+        Inner join uob_audittype on uob_audit.CORAUDITTYPEKY = uob_audittype.CORAUDITTYPEKY
+        Inner join uob_message on uob_message.MSGCODE = uob_audittype.AUDITFUNCTIONENUM
+
+ )
+
+ select * from DNAuditDetails
--- models/uobData/UserDetails.sql
+++ models/uobData/UserDetails.sql
@@ -15,23 +15,25 @@ DNUserDetails as (
            uob_arcprincipal.ARCPRINCIPALKY as P_ARCPRINCIPALKY,
            uob_arcprincipal."NAME" as P_PRINCIPALNAME,
            uob_customer.BNKCUSTOMERKY as C_BNKCUSTOMERKY,
-           uob_customer.BNKORGKY as C_BNKORGKY,
            uob_customer.CORORGKYFI as C_CORORGKYFI,
            uob_customer.ARCPRINCIPALKY as C_ARCPRINCIPALKY,
            uob_customer.CUSTOMERID as C_CUSTOMERID,
            uob_customer.UPDATEDTTM as C_UPDATEDTTM,
+           uob_customer.ISSKIPSYNCATLOGIN as C_ISSKIPSYNCATLOGIN,
            uob_user.BNKUSERKY as U_BNKUSERKY,
            uob_user.ENROLLDTTM as U_ENROLLDTTM,
            uob_user.UPDATEDTTM as U_UPDATEDTTM,
            uob_user.USERACCESSTYPECD as U_USERACCESSTYPECD,
            uob_user.ISCONFSCRNENABLED as U_ISCONFSCRNENABLED,
            uob_user.ISACTIVE as U_ISACTIVE,
-           uob_user.ISDELETED as U_ISDELETED
+           uob_user.ISDELETED as U_ISDELETED,
+           uob_user.DEFAULTSCREEN as U_DEFAULTSCREEN,
+           uob_user.ISCMPSYSADMIN as U_ISCMPSYSADMIN
            
         from uob_user
-        Left join uob_customer  on uob_customer.BNKCUSTOMERKY = uob_user.BNKCUSTOMERKY
-        Left join uob_affiliate on uob_affiliate.CORORGKY =  uob_customer.CORORGKYFI
-        Left join uob_arcprincipal on uob_user.ARCPRINCIPALKY = uob_arcprincipal.ARCPRINCIPALKY                             
+        Inner join uob_customer  on uob_customer.BNKCUSTOMERKY = uob_user.BNKCUSTOMERKY
+        Inner join uob_affiliate on uob_affiliate.CORORGKY =  uob_customer.CORORGKYFI
+        Inner join uob_arcprincipal on uob_user.ARCPRINCIPALKY = uob_arcprincipal.ARCPRINCIPALKY                         
     )
 
 select * from DNUserDetails
